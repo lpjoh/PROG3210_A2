06538011e93999f8fb71f2309279a7c341c122c8
lpjoh <devcrossfrog@gmail.com>
2024-11-13 14:58:45 -0500

    first commit

---
+++ app/src/main/java/com/example/prog3210_a2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.prog3210_a2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_search);
+    }
+}
